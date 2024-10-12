e0598d7a860b58555ba6718f29e2d0d644f80e70
Hp <Hp@Trung>
2024-10-12 12:42:48 +0700

    up dự án

---
+++ QLKTX/src/main/java/com/QLKTX/QlktxApplication.java
@@ -0,0 +1,13 @@
+package com.QLKTX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlktxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlktxApplication.class, args);
+	}
+
+}
